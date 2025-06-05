31d982af4aa84f355193b9317e58ed73ad25d1ec
Carlos Ramírez <39138003+rmansillacarlos@users.noreply.github.com>
2025-06-05 02:52:49 -0400

    styles Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import Header from '@/components/chat/Header'
+import Avatar from '@/components/ui/Avatar'
 import Button from '@/components/ui/Button'
 import Card from '@/components/ui/Card'
 import PropmtInput from '@/components/ui/PropmtInput'
@@ -8,20 +9,21 @@ export default function Home() {
   return (
     <div className='max-w-5xl'>
       <Header />
-      <div className="mb-4 py-4 px-28 h-full flex flex-col justify-between bg-stone-100 rounded-2xl">
+      <div className="mb-4 py-4 px-28 h-[calc(100vh-36px-4rem)] flex flex-col justify-between bg-stone-100 rounded-2xl">
         <div className="mb-4 flex flex-col flex-1 justify-center gap-4">
-          <div className="mx-auto pb-20 text-center">
-            <h1 className="text-2xl font-bold mb-2">Hi, there</h1>
+          <div className="mx-auto flex flex-col gap-2 justify-center items-center pb-20 text-center">
+            <Avatar src="https://gravatar.com/avatar/6717be7d9ec2673e848f24b7979555a1?s=200&d=retro&r=x" size="lg" className="mb-2" />
+            <h1 className="text-2xl font-bold">Hi, there</h1>
             <p>
               Next.js is a top front-end framework wich is most preferred by companies.
             </p>
           </div>
           <div className="flex flex-row gap-4">
-            <Card variant='primary'>
+            <Card variant="primary">
               <div className="flex flex-col justify-between">
                 <div className="mb-3 flex justify-between items-center">
                   <h3 className="font-bold">Sam Lee</h3>
-                  <button className='py-1 px-3 bg-blue-500 text-white text-xs rounded-lg'>Data assistant</button>
+                  <button className="py-1 px-3 bg-blue-500 text-white text-xs rounded-lg">Data assistant</button>
                 </div>
                 Labore in tempor dolor duis id occaecat sunt.
               </div>
@@ -40,22 +42,22 @@ export default function Home() {
             </Card>
           </div>
           <div className="flex gap-4 justify-center">
-            <Button variant='secondary'>
+            <Button variant="secondary">
               Connect Calendar
             </Button>
-            <Button variant='secondary'>
+            <Button variant="secondary">
               Demo Task
             </Button>
-            <Button variant='secondary'>
+            <Button variant="secondary">
               Browse Integrations
             </Button>
-            <Button variant='secondary'>
+            <Button variant="secondary">
               Shared in Notes
             </Button>
           </div>
         </div>
         {/* prompt input */}
-        <div className='mb-4'>
+        <div className="mb-4">
           <PropmtInput />
           <p className="mt-4 text-center text-sm text-gray-500">Anim consectetur elit laboris ullamco mollit minim id proident <a href="" className='text-black underline'>dolore commodo et.</a></p>  
         </div>

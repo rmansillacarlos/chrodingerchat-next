98d00d9d64e37910ca81e623ee01ad6e2db8aa5e
Carlos Ramírez <39138003+rmansillacarlos@users.noreply.github.com>
2025-06-05 02:52:12 -0400

    styles (Prompt Input)

--- src/components/ui/PropmtInput.tsx
+++ src/components/ui/PropmtInput.tsx
@@ -1,17 +1,19 @@
 import Button from "@/components/ui/Button";
+import Icon from "@/components/ui/Icon";
 
 export default function PropmtInput() {
   return (
-    <div className="p-4.5 flex flex-col bg-white border border-gray-300 shadow-xl shadow-gray-200 rounded-4xl">
-      <div className="mb-4">
+    <div className="px-4.5 py-4 flex flex-col bg-white border border-gray-300 shadow-xl shadow-gray-200 rounded-4xl">
+      <div className="flex gap-2 items-center mb-4">
+        <Icon icon="lucide:wand-sparkles" className="text-gray-500" />
         <input type="text" placeholder="Ask me anything" className="w-full outline-none" />
       </div>
       <div className="flex justify-between">
-          <Button variant="secondary">Model: Gara3</Button>
+        <Button variant="secondary">Model: Gara v3<Icon icon="heroicons:chevron-down" size="12px" /></Button>
         <div className="flex gap-2">
-          <Button variant="secondary">Attach</Button>
-          <Button variant="secondary">Voice</Button>
-          <Button className="ml-2">Send</Button>
+          <Button variant="secondary"><Icon icon="heroicons:link" />Attach</Button>
+          <Button variant="secondary"><Icon icon="heroicons:microphone" />Voice</Button>
+          <Button className="ml-2"><Icon icon="heroicons:arrow-up" />Send</Button>
         </div>
       </div>
     </div>
